3c6e8d58ad880ed8a1882c7aed3c21e63997f18d
Mekhi-M <mekhim0731@gmail.com>
2024-06-18 09:07:03 -0400

    short answer + fetch start

--- src/fetch-functions.js
+++ src/fetch-functions.js
@@ -1,8 +1,17 @@
-export const getFirstThreeFantasyBooks = () => {
+export const getFirstThreeFantasyBooks = async () => {
+  const url = `https://openlibrary.org/subjects/fantasy.json`;
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Failed to get fantasy books");
+    }
+  } catch (error) {
+    console.warn(error.message);
+    return null;
+  }
 };
 
-export const getAuthor = () => {
-};
+export const getAuthor = () => {};
 
-export const createNewUser = () => {
-}
+export const createNewUser = () => {};
